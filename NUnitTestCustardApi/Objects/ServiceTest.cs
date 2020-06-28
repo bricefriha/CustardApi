dc42c825a8422b4377cfcc555341a35ef3ba8191
Brice Friha <brice.friha@aol.fr>
2020-06-28 18:25:04 +0200

    Run unit tests

--- NUnitTestCustardApi/Objects/ServiceTest.cs
+++ NUnitTestCustardApi/Objects/ServiceTest.cs
@@ -185,5 +185,85 @@ namespace NUnitTestCustardApi
             // Assert
             Assert.AreEqual(Expectation.Status, actualResult.Status);
         }
+        // Post Method
+        //
+        // With a body no params no token
+        [Test]
+        public async Task PostMethodWithBodyString()
+        {
+            // Arrange
+
+            string body = "{ \"email\": \"brice.friha@outlook.com\", \"password\": \"pwd\" }";
+
+            // Act
+            string actualResult = await _service.ExecutePost ( "users", "authenticate", null,  body);
+
+            // Assert
+            Console.WriteLine(actualResult);
+            Assert.Pass();
+        }
+        // Post Method
+        // With a body a token but no params 
+        [Test]
+        public async Task GetMethodWithTokenString()
+        {
+            // Arrange
+            IDictionary<string, string> headers = new Dictionary<string, string>() ;
+
+            headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWUwZTI1NTU2Mjk0YzJjNzBlZTEyOGIiLCJpYXQiOjE1OTE3OTgwMDh9.dPiJu9zBRWEAOs-9DrPo9MtJrNt3HgNAlqtEt8QclMQ");
+
+
+            // Act
+            string actualResult = await _service.ExecuteGet ( "todolists", null, headers);
+
+            // Assert
+            Console.WriteLine(actualResult);
+            Assert.Pass();
+        }
+        // Put Method
+        [Test]
+        public async Task PutMethodWithTokenString()
+        {
+            // Arrange
+            string body = "{ \"title\": \"Workout\" }";
+            IDictionary<string, string> headers = new Dictionary<string, string>();
+
+            string[] parameters = { "5ee24eff796d9519fcc1b25e" };
+
+            headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWUyNGVlMzc5NmQ5NTE5ZmNjMWIyNWQiLCJpYXQiOjE1OTE4ODk2MzV9.tpUBOo3D0JvS0XOQzGdnag4olb8HFOZEFmVAoEINYUU");
+
+
+            // Act
+            string actualResult = await _service.ExecutePut ("todolists", "rename", headers, body, parameters);
+
+            // Assert
+            Console.WriteLine(actualResult);
+            Assert.Pass();
+        }
+
+        // Delete Method
+        [Test]
+        public async Task DeleteMethodWithTokenString()
+        {
+            // Arrange
+            ///
+            /// Prepare the header
+            IDictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWUyNGVlMzc5NmQ5NTE5ZmNjMWIyNWQiLCJpYXQiOjE1OTE4ODk2MzV9.tpUBOo3D0JvS0XOQzGdnag4olb8HFOZEFmVAoEINYUU");
+            ///
+            /// Create the item that we gonna delete later on
+            string body = "{ \"title\": \"Workout\" }";
+            Todolist itemToDelete = await _service.ExecutePost<Todolist>("todolists", "create", headers, body);
+            /// 
+            /// Put the id as parameters of the delete method
+            string[] parameters = { itemToDelete.Id };
+
+            // Act
+            string actualResult = await _service.ExecuteDelete ("todolists", null, headers, null, parameters);
+
+            // Assert
+            Console.WriteLine(actualResult);
+            Assert.Pass();
+        }
     }
 }
