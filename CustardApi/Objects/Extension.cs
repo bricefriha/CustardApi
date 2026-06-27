a4c855940f540249942b4278c3e41471257b41ed
bricefriha <contact@bricefriha.dev>
2026-06-27 14:31:23 +0100

    add more argument to the extension

--- CustardApi/Objects/Extension.cs
+++ CustardApi/Objects/Extension.cs
@@ -5,15 +5,22 @@ namespace CustardApi.Objects;
 
 public static class Extension
 {
-    public static IServiceCollection AddCustard(this IServiceCollection services, string baseUrl)
+    public static IServiceCollection AddCustard(this IServiceCollection services, string baseUrl, string host, int port = 80, bool sslCertificate = false)
     {
         services.AddHttpClient("custard", client =>
         {
+            var scheme = sslCertificate ? "https" : "http";
+            var baseUrl = $"{scheme}://{host}{(port == 80 ? "/" : ":" + port + "/")}";
             client.BaseAddress = new Uri(baseUrl);
-            client.Timeout = TimeSpan.FromSeconds(30);
         });
 
-        services.AddSingleton<Service>();
+        services.AddSingleton<Service>(sp =>
+            new Service(
+                host: host,
+                port: port,
+                sslCertificate: sslCertificate,
+                handler: null // or a custom handler if you need one
+            ));
 
         return services;
     }
