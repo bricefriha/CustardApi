2ba3cb6bc8ac81e78f6b0ee5162e1ac9189723ef
bricefriha <contact@bricefriha.dev>
2026-06-27 15:00:51 +0100

    Add InternalHttpClientFactory with optional SSL validation
    Introduced InternalHttpClientFactory in CustardApi.Objects. This class implements IHttpClientFactory and enables creation of HttpClient instances with configurable SSL certificate validation, allowing bypass of certificate checks when needed.

---
+++ CustardApi/Objects/InternalHttpClientFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+
+namespace CustardApi.Objects;
+
+internal class InternalHttpClientFactory : IHttpClientFactory
+{
+    private readonly bool _ssl;
+
+    public InternalHttpClientFactory(bool ssl)
+    {
+        _ssl = ssl;
+    }
+
+    public HttpClient CreateClient(string name)
+    {
+        var handler = new HttpClientHandler();
+
+        if (!_ssl)
+            handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+
+        return new HttpClient(handler);
+    }
+}
