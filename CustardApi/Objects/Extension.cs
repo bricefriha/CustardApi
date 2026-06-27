12d3e973ac185c86a06def3698413dc01781e708
bricefriha <contact@bricefriha.dev>
2026-06-27 14:11:15 +0100

    create an extension

---
+++ CustardApi/Objects/Extension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CustardApi.Objects;
+
+public static class Extension
+{
+    public static IServiceCollection AddCustard(this IServiceCollection services, string baseUrl)
+    {
+        services.AddHttpClient("custard", client =>
+        {
+            client.BaseAddress = new Uri(baseUrl);
+            client.Timeout = TimeSpan.FromSeconds(30);
+        });
+
+        services.AddSingleton<Service>();
+
+        return services;
+    }
+}
